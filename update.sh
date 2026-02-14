a29154032bc8543841199dc363bd387324b72412
SushiCannibale <sushi.cannibale@gmail.com>
2026-02-14 15:22:38 +0100

    update script rework. Need to be tested
    Signed-off-by: SushiCannibale <sushi.cannibale@gmail.com>

--- update.sh
+++ update.sh
@@ -1,44 +1,43 @@
 #!/bin/sh
 
-source="$HOME/.config"
-dest="src"
+DESTINATION="$(pwd)/src"
 
-[ ! -d "$dest" ] && mkdir "$dest"
+[ ! -d "$DESTINATION" ] && mkdir "$dest"
 
-### BTOP ###
-[ ! -d "$dest"/btop/ ] && mkdir "$dest"/btop/
-cp -r "$source"/btop/themes/ "$dest"/btop/
-cp -r "$source"/btop/btop.conf "$dest"/btop/
+function save {
+	[ -z "$1" ] && echo "Source not set!" && exit 1
+	src="$1"
+	echo "$src -> $DESTINATION/$src"
+	mkdir -p "$DESTINATION/$src"
+	rsync -lrv "$src" "$DESTINATION/$src" --filter=':- .gitignore'
+	# realsrc="$(realpath $src)"
+	# echo "$realsrc -> $realdst"
+}
 
-### DUNST ###
-cp -r "$source"/dunst/ "$dest"/
+save ".config/btop"
 
-### FUZZEL ###
-cp -r "$source"/fuzzel/ "$dest"/
-
-### HYPR ###
-cp -r "$source"/hypr/ "$dest"/
-
-### IMV ###
-cp -r "$source"/imv/ "$dest"/
-
-### KITTY ###
-cp -r "$source"/kitty/ "$dest"/
-
-### MICRO ###
-[ ! -d "$dest"/micro/ ] && mkdir "$dest"/micro/
-cp -r "$source"/micro/settings.json "$dest"/micro/
-cp -r "$source"/micro/bindings.json "$dest"/micro/
-cp -r "$source"/micro/colorschemes/ "$dest"/micro/
-
-### SWWW ###
-cp -r "$source"/swww "$dest"/
-
-### WAYBAR ###
-cp -r "$source"/waybar "$dest"/
-
-### STARSHIP ###
-cp -r "$source"/starship.toml "$dest"/
-
-### ZSHRC ###
-cp -r "$HOME/.zshrc" "$dest"/
+# ### BTOP ###
+# [ ! -d "$dest"/btop/ ] && mkdir "$dest"/btop/
+# cp -r "$source"/btop/themes/ "$dest"/btop/
+# cp -r "$source"/btop/btop.conf "$dest"/btop/
+# 
+# cp -r "$source"/dunst/ "$dest"/
+# cp -r "$source"/fuzzel/ "$dest"/
+# cp -r "$source"/hypr/ "$dest"/
+# cp -r "$source"/imv/ "$dest"/
+# cp -r "$source"/kitty/ "$dest"/
+# 
+# ### MICRO ###
+# [ ! -d "$dest"/micro/ ] && mkdir "$dest"/micro/
+# cp -r "$source"/micro/settings.json "$dest"/micro/
+# cp -r "$source"/micro/bindings.json "$dest"/micro/
+# cp -r "$source"/micro/colorschemes/ "$dest"/micro/
+# 
+# cp -r "$source"/swww "$dest"/
+# cp -r "$source"/waybar "$dest"/
+# cp -r "$source"/starship.toml "$dest"/
+# 
+# cp -r "$HOME/.zshrc" "$dest"/
+# 
+# [ ! -d "$dest"/VSCodium/User ] && mkdir -p "$dest"/VSCodium/User/
+# cp "$source"/VSCodium/User/settings.json "$dest"/VSCodium/User
