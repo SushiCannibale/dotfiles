54b21d54db144f20ff8c54f630cd0b1638277730
SushiCannibale <sushi.cannibale@gmail.com>
2026-02-14 16:52:09 +0100

    Fix: update script + opt-in files instead of opt-out
    Signed-off-by: SushiCannibale <sushi.cannibale@gmail.com>

--- update.sh
+++ update.sh
@@ -1,40 +1,8 @@
 #!/bin/sh
 
-DESTINATION="$(pwd)/src"
+[ ! $# = 1 ] && echo "Missing target!" && exit 1
+to="$1"
+[ ! -d "$(pwd)/$to" ] && mkdir "$(pwd)/$to"
 
-[ ! -d "$DESTINATION" ] && mkdir "$dest"
-
-function save {
-	[ -z "$1" ] && echo "Source not set!" && exit 1
-	src="$1"
-	mkdir -p "$DESTINATION/$src"
-	rsync -lrv "$src" "$DESTINATION/$src" --filter=':- .gitignore'
-}
-
-save ".config/btop"
-
-# ### BTOP ###
-# [ ! -d "$dest"/btop/ ] && mkdir "$dest"/btop/
-# cp -r "$source"/btop/themes/ "$dest"/btop/
-# cp -r "$source"/btop/btop.conf "$dest"/btop/
-# 
-# cp -r "$source"/dunst/ "$dest"/
-# cp -r "$source"/fuzzel/ "$dest"/
-# cp -r "$source"/hypr/ "$dest"/
-# cp -r "$source"/imv/ "$dest"/
-# cp -r "$source"/kitty/ "$dest"/
-# 
-# ### MICRO ###
-# [ ! -d "$dest"/micro/ ] && mkdir "$dest"/micro/
-# cp -r "$source"/micro/settings.json "$dest"/micro/
-# cp -r "$source"/micro/bindings.json "$dest"/micro/
-# cp -r "$source"/micro/colorschemes/ "$dest"/micro/
-# 
-# cp -r "$source"/swww "$dest"/
-# cp -r "$source"/waybar "$dest"/
-# cp -r "$source"/starship.toml "$dest"/
-# 
-# cp -r "$HOME/.zshrc" "$dest"/
-# 
-# [ ! -d "$dest"/VSCodium/User ] && mkdir -p "$dest"/VSCodium/User/
-# cp "$source"/VSCodium/User/settings.json "$dest"/VSCodium/User
+rsync -rav "$HOME" "$to" --files-from="files.txt"
+rsync -rav "$HOME/.zshrc" "$to"
